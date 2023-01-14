39bb083784f3d066ddb33c1151cbb137e3796fe3
Muhammad Ayaz Hasan <ayazhasan97@gmail.com>
2023-01-14 16:11:43 +0500

    Add files via upload

---
+++ JAVA/Sum.java
@@ -0,0 +1,20 @@
+
+//JAVA PROGRAM TO ADD TWO NUMBERS
+import java.util.Scanner;
+public class Sum {
+
+	public static void main(String[] args) {
+		
+		Scanner s1=new Scanner(System.in);
+		
+		System.out.println("Enter Value a");
+		int a=s1.nextInt();
+		System.out.println("Enter Value b");
+		int b=s1.nextInt();
+		int c=a+b;
+		System.out.println("Sum Of Two No is:"+c);
+		
+
+	}
+
+}
