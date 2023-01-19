ee3336e04bfcd488dcd536f6d28c5629ca803bac
Muhammad Ayaz Hasan <ayazhasan97@gmail.com>
2023-01-19 13:14:44 +0500

    Create Basic_1.py

---
+++ PYTHON/Basic_1.py
@@ -0,0 +1,10 @@
+#Basic Python Program to understand the Syntax of Print ,variables,escape sequence and some other function
+
+print("I am a Programmer") # print syntax
+
+print("Name:ABC\nEmail:abc123@gmail.com") #(\n)escape sequence that will change the line 
+
+print("I am a Programmer",3,5)    #MULITPLE VALUES IN SINGLE PRINT STATEMENT
+
+print("hello",5,6,sep="-")  #Seprator specify how to seprate the object 
+
