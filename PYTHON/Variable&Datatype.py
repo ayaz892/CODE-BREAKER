e70d373fa48b77853c458707bd7d866f7d20f7cb
Muhammad Ayaz Hasan <ayazhasan97@gmail.com>
2023-01-22 16:11:35 +0500

    Create Variable&Datatype.py

---
+++ PYTHON/Variable&Datatype.py
@@ -0,0 +1,14 @@
+#Data Types &Variables
+#===========================================
+
+a=1                     #integer
+b="I am a Programmer"   #String
+c=True                  #Boolean
+d=None                  #None 
+e=complex(5,6)          #complex
+
+print(a,"\n",b ,"\n",c,"\n",d)
+
+
+print("Type of All Variables are",type(a),"\n",type(b),"\n",type(c),"\n",type(d),"\n",type(e))       #Finding type of Variable
+
