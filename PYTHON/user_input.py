621b06f3669b73a44c1cd31389dc63bc62660d2a
Muhammad Ayaz Hasan <ayazhasan97@gmail.com>
2023-01-22 19:20:27 +0500

    Create user_input.py

---
+++ PYTHON/user_input.py
@@ -0,0 +1,19 @@
+# User Input in Python
+#==================================================
+#1st way
+a=input()
+print("I am a",a)
+
+#2nd way
+b=input("Enter Your Name Here:")
+print("I am a",b)
+
+#3rd way
+c=input("Enter 1st no here:")
+d=input("Enter 2nd no here:")
+print("Sum is ",int(c)+int(d))
+
+#4th way
+d=float(input())
+e=int(input())
+print(d,e)
