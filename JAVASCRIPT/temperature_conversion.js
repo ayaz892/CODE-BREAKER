ce77c50e71fbf840f76080c00a9d0647cd6152d3
Muhammad Ayaz Hasan <ayazhasan97@gmail.com>
2024-01-05 11:35:50 +0500

    Update temperature_conversion.js

--- JAVASCRIPT/temperature_conversion.js
+++ JAVASCRIPT/temperature_conversion.js
@@ -1,1 +1,18 @@
+//initilaizing current forecast
+const kelvin=293;
+//subtract the 273 for convert  into celsius
+let celsius=kelvin-273;
 
+//calculate fahrenheit
+let fahrenheit;
+fahrenheit=celsius *(9/5) + 32;
+//convert into whole no from decimal
+
+fahrenheit=Math.floor(fahrenheit);
+//print the temperature in fahrenheit
+console.log("The Temperature is " + fahrenheit + " degrees Fahrenheit");
+
+//convert from celcius to Newton
+let newton=celsius*(33/100);
+newton=Math.floor(newton);
+console.log(`The temperature is ${newton} degrees newton.`);
