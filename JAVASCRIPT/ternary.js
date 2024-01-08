db9aa94b9d91920636038a8075ffb093c0c4f5a3
Muhammad Ayaz Hasan <ayazhasan97@gmail.com>
2024-01-08 19:09:53 +0500

    Create ternary.js

---
+++ JAVASCRIPT/ternary.js
@@ -0,0 +1,15 @@
+
+/*Ternary Operator
+In the spirit of using short-hand syntax, we can use a ternary operator to simplify an if...else statement.*/
+let isLocked = false;
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+
+let favoritePhrase = 'Love That!';
+
+favoritePhrase === 'Love That!'?console.log('I love that!'):console.log("I don't love that!");
