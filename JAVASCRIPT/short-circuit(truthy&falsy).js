85c49f5684a22103cb428298e92b14b08675097a
Muhammad Ayaz Hasan <ayazhasan97@gmail.com>
2024-01-08 12:38:32 +0500

    Create short-circuit(truthy&falsy).js

---
+++ JAVASCRIPT/short-circuit(truthy&falsy).js
@@ -0,0 +1,7 @@
+let tool = 'marker';
+
+// Use short circuit evaluation to assign  writingUtensil variable below:
+fir it will check left hand side if tool have no value assign will assign pen otherwise it will show the tool values
+let writingUtensil=tool || 'pen'
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
