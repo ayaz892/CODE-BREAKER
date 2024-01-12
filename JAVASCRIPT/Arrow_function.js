11bca9bc04c512cf509d030976fbb5847464abae
Muhammad Ayaz Hasan <ayazhasan97@gmail.com>
2024-01-12 16:18:22 +0500

    Create Arrow_function.js

---
+++ JAVASCRIPT/Arrow_function.js
@@ -0,0 +1,13 @@
+/*Arrow Functions
+ES6 introduced arrow function syntax, a shorter way to write functions by using the special “fat arrow” () => notation.
+
+Arrow functions remove the need to type out the keyword function every time you need to create a function.
+Instead, you first include the parameters inside the ( ) and then add an arrow => that points to the function body surrounded in { } like this:*/
+
+const plantNeedsWater = (day)=> {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
