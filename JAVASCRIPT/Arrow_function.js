9ae1e140ea19f648d901514aec3aecbacd98300d
Muhammad Ayaz Hasan <ayazhasan97@gmail.com>
2024-01-14 11:21:08 +0500

    Update Arrow_function.js

--- JAVASCRIPT/Arrow_function.js
+++ JAVASCRIPT/Arrow_function.js
@@ -11,3 +11,10 @@ const plantNeedsWater = (day)=> {
     return false;
   }
 };
+
+//Functions that take only a single parameter do not need that parameter to be enclosed in parentheses.
+//However, if a function takes zero or multiple parameters, parentheses are required.
+
+
+const plantNeedsWater = day => day === 'Wednesday' ? true : false;          //instead of return we direct use variable
+
