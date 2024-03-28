7ac62efb0e43280626b6623cf1c250586c3b1244
Muhammad Ayaz Hasan <ayazhasan97@gmail.com>
2024-03-28 19:59:37 +0500

    Update BREAKINGDB.sql

--- HACKERRANK/BREAKINGDB.sql
+++ HACKERRANK/BREAKINGDB.sql
@@ -1,1 +1,7 @@
+Q:Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
 
+The CITY table is described as follows:
+
+ANS:SELECT NAME FROM CITY WHERE COUNTRYCODE='USA' AND POPULATION>120000
+
+Q:
