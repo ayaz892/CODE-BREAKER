9ffe0b46999aa70f7d65fd8c94a0a8e66ab41417
Muhammad Ayaz Hasan <ayazhasan97@gmail.com>
2025-03-11 12:38:00 +0500

    Create string_indexing.py

---
+++ PYTHON/string_indexing.py
@@ -0,0 +1,10 @@
+#Try using the index method yourself now!
+
+
+#Using the index method, find out the position of "x" in "supercalifragilisticexpialidocious".
+word = "supercalifragilisticexpialidocious"
+print(word.index("x"))
+
+#output
+#Here is your output:
+#21
